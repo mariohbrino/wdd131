c3ced81215f76ecbf1e23c7b1f2ae51bb24c32a1
Mario Brino <mariobrino@gmail.com>
2025-09-07 02:11:54 -0600

    chore: week 2 learning activity - handling dom events

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,13 +1,20 @@
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
+const notification = document.querySelector("#notification");
 
-document.getElementById("add-chapter").addEventListener("click", function(event){
+button.addEventListener("click", event => {
   event.preventDefault()
 });
 
+input.addEventListener("input", (event) => {
+    if (event.target.value.trim() !== "") {
+        notification.innerHTML = "";
+    }
+});
+
 const addChapter = () => {
-    if (input.value) {
+    if (input.value.trim() !== "") {
         const li = document.createElement("li");
         const deleteButton = document.createElement("button");
         const span = document.createElement("span");
@@ -18,10 +25,15 @@ const addChapter = () => {
         deleteButton.onclick = deleteItem;
         li.append(deleteButton);
         list.append(li);
-    };
+    } else {
+        notification.innerHTML = "No chapter defined, please fill the field.";
+    }
+    input.focus();
 };
 
 const deleteItem = (event) => {
     const parent = event.target.parentNode;
+    // console.log(parent.querySelector("span"));
     parent.remove();
+    input.focus();
 };
